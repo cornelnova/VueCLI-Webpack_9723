ab34daef4ac4e4c250b883fe1454fa6023d70c54
cornelnova <70184917+cornelnova@users.noreply.github.com>
2020-11-15 23:17:40 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
+import VueProgressBar from 'vue-progressbar'
 import router from './router'
 
 Vue.config.productionTip = false
+
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options)
 
 new Vue({
   vuetify,
